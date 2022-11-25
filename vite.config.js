2841982fbdb3182857e3a13e4ec97d03a3bbc03d
Mateus <mateuscolvr@gmail.com>
2022-11-25 06:52:59 -0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import rescript from '@jihchi/vite-plugin-rescript'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), rescript()]
+})
